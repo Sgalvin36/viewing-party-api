a8c40f4412f1df7a95344634cbd08fc75a88f696
Shane Galvin <Sgalvin36@gmail.com>
2024-10-12 17:32:48 -0600

    Incorporated factory into viewing_party

--- spec/requests/api/v1/viewing_party_request_spec.rb
+++ spec/requests/api/v1/viewing_party_request_spec.rb
@@ -3,46 +3,42 @@ require "rails_helper"
 RSpec.describe "ViewingParty API", type: :request do
     describe "Create ViewingParty Endpoint" do
         it "passes authentication" do
-            user = User.create(name: "Joey", username: "Friend#1", password: "Sitcomking")
-            user2 = User.create(name: "Joey", username: "Friend#2", password: "Friendsthebest")
-            user3 = User.create(name: "Joey", username: "Friend#3", password: "Onlyfriends")
+            users = create_list(:user, 3)
             params = {
                 name: "Friends for ever and ever",
                 start_time: "properly formatted start time",
                 end_time: "properly formatted end time",
                 movie_id: 456,
                 movie_title: "Princess Bride",
-                api_key: user.api_key,
-                user_id: user.id,
-                users: [user2.id, user3.id]
+                api_key: users[0].api_key,
+                user_id: users[0].id,
+                users: [users[1].id, users[2].id]
             }
 
             headers = { 
                 "CONTENT_TYPE" => "application/json",
-                "Authorization" => user.api_key
+                "Authorization" => users[0].api_key
             }
             post api_v1_viewing_parties_path(params), headers: headers
             expect(response).to be_successful
         end
 
         it "can create a viewing party" do
-            user = User.create(name: "Joey", username: "Friend#1", password: "Sitcomking")
-            user2 = User.create(name: "Joey", username: "Friend#2", password: "Friendsthebest")
-            user3 = User.create(name: "Joey", username: "Friend#3", password: "Onlyfriends")
+            users = create_list(:user, 3)
             params = {
                 name: "Friends for ever and ever",
                 start_time: "properly formatted start time",
                 end_time: "properly formatted end time",
                 movie_id: 456,
                 movie_title: "Princess Bride",
-                api_key: user.api_key,
-                user_id: user.id,
-                users: [user2.id, user3.id]
+                api_key: users[0].api_key,
+                user_id: users[0].id,
+                users: [users[1].id, users[2].id]
             }
 
             headers = { 
                 "CONTENT_TYPE" => "application/json",
-                "Authorization" => user.api_key
+                "Authorization" => users[0].api_key
             }
 
             post api_v1_viewing_parties_path(params), headers: headers
@@ -61,18 +57,16 @@ RSpec.describe "ViewingParty API", type: :request do
         end
 
         it "returns an error if no provided with an api-key" do
-            user = User.create(name: "Joey", username: "Friend#1", password: "Sitcomking")
-            user2 = User.create(name: "Joey", username: "Friend#2", password: "Friendsthebest")
-            user3 = User.create(name: "Joey", username: "Friend#3", password: "Onlyfriends")
+            users = create_list(:user, 3)
             params = {
                 name: "Friends for ever and ever",
                 start_time: "properly formatted start time",
                 end_time: "properly formatted end time",
                 movie_id: 456,
                 movie_title: "Princess Bride",
-                api_key: user.api_key,
-                user_id: user.id,
-                users: [user2.id, user3.id]
+                api_key: users[0].api_key,
+                user_id: users[0].id,
+                users: [users[1].id, users[2].id]
             }
 
             headers = { 
@@ -89,22 +83,20 @@ RSpec.describe "ViewingParty API", type: :request do
         end
 
         it "returns an error if not provided with all parameters" do
-            user = User.create(name: "Joey", username: "Friend#1", password: "Sitcomking")
-            user2 = User.create(name: "Joey", username: "Friend#2", password: "Friendsthebest")
-            user3 = User.create(name: "Joey", username: "Friend#3", password: "Onlyfriends")
+            users = create_list(:user, 3)
             params = {
                 name: "Friends for ever and ever",
                 start_time: "properly formatted start time",
                 end_time: "properly formatted end time",
                 movie_title: "Princess Bride",
-                api_key: user.api_key,
-                user_id: user.id,
-                users: [user2.id, user3.id]
+                api_key: users[0].api_key,
+                user_id: users[0].id,
+                users: [users[1].id, users[2].id]
             }
 
             headers = { 
                 "CONTENT_TYPE" => "application/json",
-                "Authorization" => user.api_key
+                "Authorization" => users[0].api_key
             }
             post api_v1_viewing_parties_path(params), headers: headers
             expect(response).to_not be_successful
@@ -171,23 +163,21 @@ RSpec.describe "ViewingParty API", type: :request do
         end
 
         it "handles if an invalid guest id is provided" do
-            user = User.create(name: "Joey", username: "Friend#1", password: "Sitcomking")
-            user2 = User.create(name: "Joey", username: "Friend#2", password: "Friendsthebest")
-            user3 = User.create(name: "Joey", username: "Friend#3", password: "Onlyfriends")
+            users = create_list(:user, 3)
             params = {
                 name: "Friends for ever and ever",
                 start_time: "properly formatted start time",
                 end_time: "properly formatted end time",
+                movie_id: 456,
                 movie_title: "Princess Bride",
-                movie_id: 12,
-                api_key: user.api_key,
-                user_id: user.id,
-                users: [5, user2.id, user3.id, 0]
+                api_key: users[0].api_key,
+                user_id: users[0].id,
+                users: [5, users[1].id, users[2].id, 0]
             }
 
             headers = { 
                 "CONTENT_TYPE" => "application/json",
-                "Authorization" => user.api_key
+                "Authorization" => users[0].api_key
             }
             post api_v1_viewing_parties_path(params), headers: headers
             expect(response).to be_successful
@@ -196,38 +186,34 @@ RSpec.describe "ViewingParty API", type: :request do
             json = JSON.parse(response.body, symbolize_names:true)
 
             expect(json[:data][:relationships][:users][:data].length).to eq 2
-            expect(json[:data][:relationships][:users][:data][0][:id].to_i).to eq(user2.id) 
-            expect(json[:data][:relationships][:users][:data][1][:id].to_i).to eq(user3.id)
+            expect(json[:data][:relationships][:users][:data][0][:id].to_i).to eq(users[1].id) 
+            expect(json[:data][:relationships][:users][:data][1][:id].to_i).to eq(users[2].id)
         end
     end
 
     describe "Update ViewingParty Endpoint" do
         it "can add users to existing party" do
-            user = User.create(name: "Joey", username: "Friend#1", password: "Sitcomking")
-            user2 = User.create(name: "Joey", username: "Friend#2", password: "Friendsthebest")
-            user3 = User.create(name: "Joey", username: "Friend#3", password: "Onlyfriends")
-            user4 = User.create(name: "Jacob", username: "Thing1", password: "SuessforDays")
-            user5 = User.create(name: "John", username: "Thing2", password: "RedFishBlueFish")
+            users = create_list(:user, 5)
             params = {
                 name: "Friends for ever and ever",
                 start_time: Time.now,
                 end_time: (Time.now + 4.hours),
                 movie_id: 456,
                 movie_title: "Princess Bride",
-                api_key: user.api_key,
-                user_id: user.id,
-                users: [user2.id, user3.id]
+                api_key: users[0].api_key,
+                user_id: users[0].id,
+                users: [users[2].id, users[1].id]
             }
 
             party = ViewingParty.new(params)
 
             params = {
                 "id": party.id,
-                "users": [user4.id, user5.id]
+                "users": [users[3].id, users[4].id]
             }
             headers = { 
                 "CONTENT_TYPE" => "application/json",
-                "Authorization" => user.api_key
+                "Authorization" => users[0].api_key
             }
             put api_v1_viewing_party_path(params), headers: headers
 
@@ -235,10 +221,10 @@ RSpec.describe "ViewingParty API", type: :request do
             json = JSON.parse(response.body, symbolize_names:true)
 
             expect(json[:data][:relationships][:users][:data].length).to eq 4
-            expect(json[:data][:relationships][:users][:data][0][:id].to_i).to eq(user2.id) 
-            expect(json[:data][:relationships][:users][:data][1][:id].to_i).to eq(user3.id)
-            expect(json[:data][:relationships][:users][:data][2][:id].to_i).to eq(user4.id)
-            expect(json[:data][:relationships][:users][:data][3][:id].to_i).to eq(user5.id)
+            expect(json[:data][:relationships][:users][:data][0][:id].to_i).to eq(users[1].id) 
+            expect(json[:data][:relationships][:users][:data][1][:id].to_i).to eq(users[2].id)
+            expect(json[:data][:relationships][:users][:data][2][:id].to_i).to eq(users[3].id)
+            expect(json[:data][:relationships][:users][:data][3][:id].to_i).to eq(users[4].id)
         end
     
         it "handles an invalid viewing party gently" do
